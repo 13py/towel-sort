39a8d72da5d50e8a012526dee9e865ffb38a7389
Art <pytagpro@gmail.com>
2022-08-01 20:58:30 +0500

    feat: solve this task.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix = []) {
+    let sortArray = [];
+    matrix.forEach((value, ind) => {
+        if (ind % 2 != 0) {
+            value.sort((a, b) => b - a);
+        }
+        sortArray.push(...value);
+    });
+    return sortArray;
+};
